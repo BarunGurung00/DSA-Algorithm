e7be62e6c1dffeef47845716712d56a65130e33a
barungurung00@gmail.com <barun>
2024-01-03 21:11:24 +0000

    updated bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,10 +1,24 @@
 public class BubbleSort {
     public static void main(String[] args){
         int  arr [] = {3 , 5, 12, 9, 1, 2, 4, 6, 8, 7};
-        int sortedArr[] = bubbleSort(arr);
+        
+        bubbleSort(arr);
+
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
     }
 
     public static int[] bubbleSort(int[] array){
-        
+        for(int i=0; i< array.length -1; i++){
+            for(int j=0; j<array.length-i-1; j++){
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1]= temp;
+                }
+            }
+        }
+        return array;
     }
 }
