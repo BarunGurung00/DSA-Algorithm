281eb86e4736a11e3d4194e667c68cc43e9e2a74
barungurung00@gmail.com <barun>
2024-01-03 20:23:36 +0000

    bubbleSort added

---
+++ BubbleSort.java
@@ -0,0 +1,10 @@
+public class BubbleSort {
+    public static void main(String[] args){
+        int  arr [] = {3 , 5, 12, 9, 1, 2, 4, 6, 8, 7};
+        int sortedArr[] = bubbleSort(arr);
+    }
+
+    public static int[] bubbleSort(int[] array){
+        
+    }
+}
