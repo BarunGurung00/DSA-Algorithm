6777c1218a9bb27fcbaeadf241aa4d702a8f279f
Barun <bg440@live.mdx.ac.uk>
2024-01-28 04:29:33 +0000

    QuickSort algorithm

---
+++ QuickSort.java
@@ -0,0 +1,12 @@
+public class QuickSort {
+    public static void main(String[] args){
+        int[] array = {20, 35, -15, 7, 55, 1, -22};
+        quickSort(array);
+        for(int elem: array){
+            System.out.println(elem);
+        }
+    }
+    public static void quickSort(int[] arr){
+        
+    }
+}
